ecbc346f625038dc467fce8dc603723c8d549487
usbbush <jeanbienvenusb@gmail.com>
2022-07-04 12:09:45 +0200

    Fix linters errors

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-export * as dom from './modules/dom_elements.js';
-export * as Book from './modules/book/book.js';
-export * as addBook from './modules/book/add-book.js';
-export * as removeBook from './modules/book/remove-book.js';
-export * as runOnload from './modules/book/script.js';
-export * as navigationEvent from './modules/navigation_bar/nav_event_handler.js';
-export * as date from './modules/navigation_bar/date_time.js';
-export * as mobileNav from './modules/navigation_bar/mobile_navbar_handler.js';
+import './modules/book/add-book.js';
+import './modules/book/remove-book.js';
+import './modules/book/script.js';
+import './modules/navigation_bar/nav_event_handler.js';
+import './modules/navigation_bar/date_time.js';
+import './modules/navigation_bar/mobile_navbar_handler.js';
