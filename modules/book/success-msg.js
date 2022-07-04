1434a93a3f0d1f8810fc1b86a5bee31f874bc3c2
usbbush <jeanbienvenusb@gmail.com>
2022-07-04 15:50:18 +0200

    Restore project to init state

--- modules/book/success-msg.js
+++
@@ -1,5 +0,0 @@
-const notfound = 'No Book Found, Please Add First !';
-const removed = 'Book deleted successfully.';
-const inserted = 'Book added successfully.';
-
-export { notfound, removed, inserted };
